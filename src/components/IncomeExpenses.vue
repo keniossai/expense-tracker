e4f351ae06b2e747370a47d15a394c6fef6f2161
Kenneth Ossai <kenossai1@gmail.com>
2023-11-09 21:25:41 +0000

    Create IncomeExpenses.vue

---
+++ src/components/IncomeExpenses.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="inc-exp-container">
+        <div class="">
+            <h4>Income</h4>
+            <p id="money-plus" class="money plus">+$0.00</p>
+        </div>
+        <div class="">
+            <h4>Expenses</h4>
+            <p id="money-minus" class="money minus">-$0.00</p>
+        </div>
+    </div>
+</template>
