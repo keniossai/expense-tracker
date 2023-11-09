84e428d175acb5c0245609bd117a07c710d4f7e8
Kenneth Ossai <kenossai1@gmail.com>
2023-11-09 21:25:28 +0000

    Import components

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,21 @@
 <template>
-  <div>
-    <h1>Hello VueJs</h1>
+  <Header />
+  <div class="container">
+    <Balance />
+    <IncomeExpenses />
   </div>
 </template>
+
+<script>
+  import Header from './components/Header.vue'
+  import Balance from './components/Balance.vue'
+  import IncomeExpenses from './components/IncomeExpenses.vue'
+
+  export default{
+    components: {
+      Header,
+      Balance,
+      IncomeExpenses
+    }
+  }
+</script>
